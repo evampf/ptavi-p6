4b9bb70f95b91569040de64160495bc97fc0e678
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-11-21 12:47:45 +0100

    Servidor ok

--- server.py
+++ server.py
@@ -19,20 +19,45 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     MP3 = sys.argv[3][-4:]
 
 
+    if len(sys.argv) != 4:
+        if sys.argv[3][-4:] != ".mp3":
+            sys.exit("Usage: python server.py IP port audio_file")
+
+
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
+
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
+            # Leyendo línea a línea lo que nos envía el cliente       
+            text = self.rfile.read()
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            print("El cliente nos manda " + text.decode('utf-8'))
+            LINE = text.decode('utf-8')
+            Words_LINES = LINE.split()
+            REQUEST = Words_LINES[0]
+            print("La peticion es: ", REQUEST)
+            print("Listening...")
+
+
+            if REQUEST == 'INVITE':
+                mensaje = b"SIP/2.0 100 Trying\r\n\r\n"
+                mensaje += b"SIP/2.0 180 Ring\r\n\r\n"
+                mensaje += b"SIP/2.0 200 OK\r\n\r\n"
+                self.wfile.write(mensaje)
+            elif REQUEST == 'ACK':
+                SONG = (sys.argv[3])
+                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + self.SONG
+                os.system(aEjecutar)
+            elif REQUEST == 'BYE':
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer((sys.argv[1], int(sys.argv[2])), EchoHandler)
     print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
